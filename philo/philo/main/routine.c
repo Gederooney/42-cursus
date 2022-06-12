50790794695fc36540cea454f104330bce027ddf
dev-omnigo <gyebadok61@gmail.com>
2022-06-12 10:44:28 -0400

    just a commit

--- philo/philo/main/routine.c
+++ philo/philo/main/routine.c
@@ -6,28 +6,34 @@
 /*   By: ryebadok <ryebadok@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:30:57 by ryebadok          #+#    #+#             */
-/*   Updated: 2022/06/12 10:13:56 by ryebadok         ###   ########.fr       */
+/*   Updated: 2022/06/12 10:43:32 by ryebadok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include "utils.h"
 
-// bool	ft_can_i_eat(t_thread t){
-	
-// 	return false;
-// }
+bool	ft_can_i_eat(t_thread t){
+	if (pthread_mutex_lock(&t.fs[t.p.id]))
+		return (false);
+	// if (pthread_mutex_lock(&t.fs[((t.p.id) + 1) % t.p.g->nbrp]))
+	// {
+	// 	pthread_mutex_unlock(&t.fs[t.p.id]);
+	// 	return (false);
+	// }
+	printf("philo n* %d is able to eat... ", (int)t.q[0]);
+	return true;
+}
 
 bool	ft_send_to_eat(t_thread t){
 	pthread_mutex_lock(&t.qc);
+	if (ft_can_i_eat(t)){
+		printf("philo %d is eating \n", (int)t.q[0]);
+	}
 	ft_ra(t.q, t.p.g->nbrp);
-	ft_print_arr(t.p.g->nbrp, t.q);
-	printf("\n");
-	// if (ft_can_i_eat(t)){
-	// 	printf("philo %d is eating ", (int)t.p.id);
-	// }
 	pthread_mutex_unlock(&t.qc);
-	return false;
+	pthread_mutex_unlock(&t.fs[t.p.id]);
+	return true;
 }
 
 void	*ft_routine(void *args){
@@ -36,11 +42,11 @@ void	*ft_routine(void *args){
 	t = (t_thread *)args;
 	// if (t->p.id == 0)
 	// 	ft_print_arr(t->p.g->nbre, t->q);
-	// while (1){
+	while (1){
 		ft_send_to_eat(*t);
 		
 	// 	ft_make_sleep();
 	// 	ft_make_think();
-	// }
+	}
 	return NULL;
 }
