b5e576e2f1703893802860fbae8b257139051ab6
Rony gedeon dotou Yebadokpo <ryebadok@c1r8p6.42quebec.com>
2022-12-12 14:08:51 -0500

    ex01

--- cpp-piscine/cpp00/ex01/Phone_book.cpp
+++ cpp-piscine/cpp00/ex01/Phone_book.cpp
@@ -6,7 +6,7 @@
 /*   By: ryebadok <ryebadok@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 08:11:35 by ryebadok          #+#    #+#             */
-/*   Updated: 2022/12/11 21:02:32 by ryebadok         ###   ########.fr       */
+/*   Updated: 2022/12/12 14:08:13 by ryebadok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 Phone_book::Phone_book(void){
 	count = 0;
+}
+
+int	isOnlyDigits(string str)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < str.size())
+	{
+		if (isdigit(str[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
 }
 
 int	Phone_book::add_contact(void)
@@ -44,10 +59,16 @@ string	Phone_book::parse_input(string input){
 		}
 		break;
 	}
-	if (i == input.size())
-		return ("");
-	else
-		return input.substr(i);
+	input = input.substr(i);
+	i = 0;
+	while ( i < input.size())
+	{
+		if(input[i] == ' ')
+			break;
+		i++;
+	}
+	input = input.substr(0, i);
+	return (input);
 }
 
 Contact	Phone_book::get_contact_infos(void)
@@ -63,7 +84,10 @@ Contact	Phone_book::get_contact_infos(void)
 	{
 		cout << "Enter a " + fields[step] + " ";
 		getline(cin, user_input);
-		if (user_input.empty())
+		user_input = parse_input(user_input);
+		if (!user_input.length())
+			continue;
+		if (step == 3 && !isOnlyDigits(user_input))
 			continue;
 		tmp.set_field(fields[step], user_input);
 		step++;
@@ -73,10 +97,16 @@ Contact	Phone_book::get_contact_infos(void)
 
 void	Phone_book::search_contact()
 {
-	int	i;
+	int	i, id;
 	int	limit;
+	string	user_input;
 
 	i = 0;
+	if (count == 0)
+	{
+		cout << "Aucun contact a afficher" << endl;
+		return ;
+	}
 	if (count < 8)
 		limit = count;
 	else 
@@ -86,4 +116,24 @@ void	Phone_book::search_contact()
 		contact_list[i].display();
 		i++;
 	}
+	cout << "Enter un index pour afficher le contact: ";
+	getline(cin, user_input);
+	i = 0;
+	while (!isOnlyDigits(user_input))
+	{
+		cout << "Enter un index correcte pour afficher le contact: ";
+		getline(cin, user_input);
+	}
+	id = stoi(user_input);
+	i = 0;
+	while (i < limit)
+	{
+		if (contact_list[i].get_id() == id)
+			break;
+		i++;
+	}
+	if (i == limit)
+		cout << "Aucun contact ne correspond a cet index" << endl;
+	else
+		contact_list[i].displayLineByLine();
 }
